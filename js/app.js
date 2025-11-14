d682400608b29780cc6a1ea261cb6646543dc309
MURILO ANTUNES CAMPOS DA ROCHA <25203706@dsn127m1241959.cfp127aluno.local>
2025-11-14 16:56:17 -0300

    alterando circulos de detalhe e adicionando a funcionalidade do botão de voltar, sem a animação

--- js/app.js
+++ js/app.js
@@ -20,6 +20,17 @@ proximo.onclick = () => {
     mudarSlide();
 }
 
+anterior.onclick = () => {
+    carrossel.classList.remove('proximo')
+    carrossel.classList.add('anterior')
+
+    ativado = ativado -1 < 0 ?countItem - 1 : ativado -1;
+    outro_1 =ativado - 1 < 0 ? countItem -1 : ativado - 1;
+    outro_2 = ativado + 1 >= countItem ? 0 : ativado + 1;
+
+    mudarSlideTras();
+}
+
 const mudarSlide = () => {
     let antigoItemAtivado = document.querySelector('.carrossel .item.ativado');
     if(antigoItemAtivado) antigoItemAtivado.classList.remove('ativado');
@@ -42,4 +53,28 @@ const mudarSlide = () => {
     items[ativado].classList.add('ativado');
     items[outro_1].classList.add('outro_1');
     items[outro_2].classList.add('outro_2');
-}   
+}
+
+const mudarSlideTras = () => {
+    let antigoItemAtivado = document.querySelector('.carrossel .item.ativado');
+    if(antigoItemAtivado) antigoItemAtivado.classList.remove('ativado');
+
+    let antigoOutro_1 = document.querySelector('.carrossel .item.outro_1');
+    if(antigoOutro_1) antigoOutro_1.classList.remove('outro_1');
+
+    let antigoOutro_2 = document.querySelector('.carrossel .item.outro_2');
+    if(antigoOutro_2) antigoOutro_2.classList.remove('outro_2');
+
+    items.forEach(e => {
+        e.querySelector('.image img').style.animation = 'none';
+        e.querySelector('.image figcaption').style.animation = 'none';
+        void e.offsetWidth;
+        e.querySelector('.image img').style.animation = '';
+        e.querySelector('.image figcaption').style.animation = '';
+    })
+
+
+    items[ativado].classList.add('ativado');
+    items[outro_1].classList.add('outro_1');
+    items[outro_2].classList.add('outro_2');
+}
